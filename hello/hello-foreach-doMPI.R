5b99263a76f2e31d9812289a185ad102cf352380
Glenn K. Lockwood <glock@sdsc.edu>
2013-09-08 10:23:01 -0700

    added caveat to doMPI example.  it does not work reliably

--- hello/hello-foreach-doMPI.R
+++ hello/hello-foreach-doMPI.R
@@ -19,3 +19,4 @@ cat(unlist(output.lines), sep='\n')
  
 closeCluster(cl)
 mpi.exit()
+print('doMPI is not very robust and tends to cause a lot of subtle problems.  I do not recommend its use; use doSNOW instead.')
